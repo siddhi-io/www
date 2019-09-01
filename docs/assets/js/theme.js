c360996c902fa238cc1f5db397919b91704693eb
Suhothayan <suhothayan@gmail.com>
2019-09-01 09:26:04 +0530

    List v5.1 (pre)

--- docs/assets/js/theme.js
+++ docs/assets/js/theme.js
@@ -93,8 +93,11 @@ request.onload = function () {
                     }
 
                     liElem.className = 'md-tabs__item mb-tabs__dropdown';
-                    liElem.innerHTML = '<a href="' + url + '" target="' +
-                        target + '">' + key + '</a>';
+                    var keyText = key;
+                    if (key == data.next) {
+                        keyText = key + " (pre)"
+                    }liElem.innerHTML =  '<a href="' + url + '" target="' +
+                        target + '">' + keyText + '</a>';
 
                     dropdown.insertBefore(liElem, dropdown.firstChild);
                 }
