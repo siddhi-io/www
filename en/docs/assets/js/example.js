0479244ff3c90b398fc621b37763472b767899a9
Suhothayan <suhothayan@gmail.com>
2019-11-17 11:46:29 +0530

    improve styling

--- en/docs/assets/js/example.js
+++ en/docs/assets/js/example.js
@@ -18,7 +18,7 @@
 
 
 /*
- * Following script is adding line numbers to the ballerina code blocks in the gneerated documentation
+ * Following script is adding line numbers to the siddhi code blocks in the gneerated documentation
  */
 function initCodeLineNumbers() {
     $('pre > code.ballerina, pre > code.language-ballerina').each(function() {
@@ -45,34 +45,109 @@ function initCodeLineNumbers() {
     });
 }
 
+if (typeof hljs === 'object') {
+    hljs.configure({languages: []});
+    hljs.registerLanguage('ballerina', function () {
+        return {
+            "k": "stream define function trigger table plan from partition window select group by having insert" +
+            "overwrite delete update return events into output expired current snapshot for raw of as at or and" +
+            "in on is not within with begin end null every last all first join inner outer right left full" +
+            "unidirectional years year months month weeks week days day hours hour minutes minute min seconds" +
+            "second sec milliseconds millisecond millisec false true",
+            "i": {},
+            "c": [{
+                "cN": "ballerinadoc",
+                "b": "/\\*\\*",
+                "e": "\\*/",
+                "r": 0,
+                "c": [{
+                    "cN": "ballerinadoctag",
+                    "b": "(^|\\s)@[A-Za-z]+"
+                }]
+            }, {
+                "cN": "comment",
+                "b": "//",
+                "e": "$",
+                "c": [{
+                    "b": {}
+                }, {
+                    "cN": "label",
+                    "b": "XXX",
+                    "e": "$",
+                    "eW": true,
+                    "r": 0
+                }]
+            }, {
+                "cN": "comment",
+                "b": "/\\*",
+                "e": "\\*/",
+                "c": [{
+                    "b": {}
+                }, {
+                    "cN": "label",
+                    "b": "XXX",
+                    "e": "$",
+                    "eW": true,
+                    "r": 0
+                }, "self"]
+            }, {
+                "cN": "string",
+                "b": "\"",
+                "e": "\"",
+                "i": "\\n",
+                "c": [{
+                    "b": "\\\\[\\s\\S]",
+                    "r": 0
+                }, {
+                    "cN": "constant",
+                    "b": "\\\\[abfnrtv]\\|\\\\x[0-9a-fA-F]*\\\\\\|%[-+# *.0-9]*[dioxXucsfeEgGp]",
+                    "r": 0
+                }]
+            }, {
+                "cN": "number",
+                "b": "(\\b(0b[01_]+)|\\b0[xX][a-fA-F0-9_]+|(\\b[\\d_]+(\\.[\\d_]*)?|\\.[\\d_]+)([eE][-+]?\\d+)?)[lLfF]?",
+                "r": 0
+            }, {
+                "cN": "annotation",
+                "b": "@[A-Za-z]+"
+            }, {
+                "cN": "type",
+                "b": "\\b(bool|int|float|string|long|double|object)\\b",
+                "r": 0
+            }]
+        };
+    });
+}
+
+
 function setTooltip(btn, message) {
     $(btn).attr('data-original-title', message)
         .tooltip('show');
 }
 
 function hideTooltip(btn) {
-    setTimeout(function() {
+    setTimeout(function () {
         $(btn).tooltip('hide').removeAttr('data-original-title');
     }, 1000);
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     initCodeLineNumbers();
 
 
-    document.querySelector("div.md-content > article > a.md-icon.md-content__icon").href= document.querySelector("span.info").getAttribute("dir")
+    document.querySelector("div.md-content > article > a.md-icon.md-content__icon").href = document.querySelector("span.info").getAttribute("dir")
 
-    $('.cBE-body').each(function() {
+    $('.cBE-body').each(function () {
         var lineCount = 0,
             olCount = 1;
 
-        $('.cTR', this).each(function(i, n) {
+        $('.cTR', this).each(function (i, n) {
             var $codeElem = $(n).find('td.code').get(0);
             var lines = $('> td.code', n).text().replace(/\n$/, "").trim().split(/\r\n|\r|\n/);
             var numbers = [];
 
-            $.each(lines, function(i) {
+            $.each(lines, function (i) {
                 lineCount += 1;
                 numbers.push('<span class="line-number">' + lineCount + '</span>');
             });
@@ -94,28 +169,28 @@ $(document).ready(function() {
     });
 
 
-        // register "copy to clipboard" event to elements with "copy" class
-        var clipboard = new ClipboardJS('.copy', {
-            text: function(trigger) {
-                return $('.FullCode').find('pre').text();
-            }
-        });
+    // register "copy to clipboard" event to elements with "copy" class
+    var clipboard = new ClipboardJS('.copy', {
+        text: function (trigger) {
+            return $('.FullCode').find('pre').text();
+        }
+    });
 
-        // Register events show hide tooltip on click event
-        clipboard.on('success', function(e) {
-            setTooltip(e.trigger, 'Copied!');
-            hideTooltip(e.trigger);
-        });
+    // Register events show hide tooltip on click event
+    clipboard.on('success', function (e) {
+        setTooltip(e.trigger, 'Copied!');
+        hideTooltip(e.trigger);
+    });
 
-        clipboard.on('error', function(e) {
-            setTooltip(e.trigger, 'Failed!');
-            hideTooltip(e.trigger);
-        });
+    clipboard.on('error', function (e) {
+        setTooltip(e.trigger, 'Failed!');
+        hideTooltip(e.trigger);
+    });
 
-        $('.copy').tooltip({
-            trigger: 'click',
-            placement: 'bottom'
-        });
-        $("a.copy").unbind("click");
+    $('.copy').tooltip({
+        trigger: 'click',
+        placement: 'bottom'
+    });
+    $("a.copy").unbind("click");
 
-  });
+});
