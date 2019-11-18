161ab50379f63de987090a3d63f22df8b8e08d65
WSO2 Builder <jenkins-bot@wso2.com>
2019-11-18 06:36:20 +0000

    Add updated Siddhi site files

--- en/v5.1/assets/js/theme.js
+++ en/v5.1/assets/js/theme.js
@@ -82,7 +82,7 @@ request.open('GET', docSetUrl +
  */
 if (typeof hljs === 'object') {
     hljs.registerLanguage("siddhi", function (e) {
-        var t = e.C("--", "$")
+        var t = e.C("--", "$"),
             n = {
             cN: "number",
             b: "\\b(0[bB]([01]+[01_]+[01]+|[01]+)|0[xX]([a-fA-F0-9]+[a-fA-F0-9_]+[a-fA-F0-9]+|[a-fA-F0-9]+)|(([\\d]+[\\d_]+[\\d]+|[\\d]+)(\\.([\\d]+[\\d_]+[\\d]+|[\\d]+))?|\\.([\\d]+[\\d_]+[\\d]+|[\\d]+))([eE][-+]?\\d+)?)[lLfF]?",
@@ -91,13 +91,13 @@ if (typeof hljs === 'object') {
         return {
             cI: !0,
             k: {
-                keyword: "stream define function trigger table plan from partition window select group by having " +
-                "overwrite delete update return events into output expired current snapshot for raw of as at or and " +
-                "in on is not within with begin end null every last all first join inner outer right left full " +
-                "unidirectional insert ",
-                literal: "true false years year months month weeks week days day hours hour minutes minute min seconds" +
-                "second sec milliseconds millisecond millisec",
-                built_in: "bool int float string long double object"
+                keyword: "stream define function trigger table app from partition window select group by order " +
+                "limit offset asc desc having insert delete update set return events into output expired current " +
+                "snapshot for raw of as at or and in on is not within with begin end every last all first " +
+                "join inner outer right left full unidirectional aggregation aggregate per",
+                literal: "true false null years year months month weeks week days day hours hour minutes minute min " +
+                "seconds second sec milliseconds millisecond millisec",
+                built_in: "string int long float double bool object"
             },
             i: /[<>{}*]/,
             c: [
